7730baad49f5760e1e2f506a264af2979f6086ff
mileywily <mileida.blanco.y@gmail.com>
2025-11-04 12:53:00 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,12 @@
 export const environment = {
   production: false,
 
-  apiUrlPermanencia: 'http://vmwpcon1.sidor.net/permanencias-sql-back/public/index.php/api/',
-  apiUrlSeguridad: 'http://sirprdsvvapc21.sidor.net/seguridad-back/public/index.php/api/',
- // apiUrl: 'http://sirdevapcser01.sidor.net/hcm-back/public/index.php/api/',
- //apiUrl: 'http://sirdevapcser01.sidor.net/hcm-back/public/index.php/api/',
- //apiUrl: 'http://sirdevapcser01.sidor.net/hcm-back/public/index.php/api/',
-  apiUrl: 'http://vmwpser1.sidor.net/hcm-back/public/index.php/api/',
-  apiUrlLDAP: 'http://sirprdsvvapc21.sidor.net/ldap-back/public/index.php/api/',
+  apiUrlPermanencia: 'http://xxxxxxxxx/permanencias-sql-back/public/index.php/api/',
+  apiUrlSeguridad: 'http://xxxxxxxxxxx/seguridad-back/public/index.php/api/',
+
+  apiUrl: 'http://xxxxxxxxxxxxx/hcm-back/public/index.php/api/',
+  apiUrlLDAP: 'xxxxxxxxxxxxxxxx/ldap-back/public/index.php/api/',
   //apiUrlSiss: 'http://localhost/beneficiarios-siss-back/public/index.php/api/',
-  apiUrlSiss: 'http://sirdevapcser01.sidor.net/beneficiarios-siss-back/public/index.php/api/',
+  apiUrlSiss: 'http://xxxxxxxxxxx/beneficiarios-siss-back/public/index.php/api/',
 
 };
