74509315c9b5ec0dfe12e86665a39a6d3c94482d
mileywily <mileida.blanco.y@gmail.com>
2025-11-04 12:53:56 -0400

    Replace production API URLs with placeholders
    Updated API URLs in the production environment file to use placeholder values.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,21 +4,6 @@
 //   apiUrl: 'http://localhost/hcm-back/public/index.php/api/',
 //apiUrl: 'http://sirdevapcser01.sidor.net/hcm-back/public/index.php/api/',
 
-/*export const environment = {
-  production: true,
-  apiUrl: 'http://sirdevapcser01.sidor.net/hcm-back/public/index.php/api/',
-  apiUrlLDAP: 'http://sirprdsvvapc21.sidor.net/ldap-back/public/index.php/api/',
-  apiUrlPermanencia: 'http://vmwdcon1.sidor.net/permanencias-sql-back/public/index.php/api/',
-  apiUrlSeguridad: 'http://sirprdsvvapc21.sidor.net/seguridad-back/public/index.php/api/',
-  apiUrlCargaFamiliar: 'http://sirdevapcser01.sidor.net/beneficiarios-siss-back/public/index.php/api/',
-  apiUrlSiss: 'http://sirdevapcser01.sidor.net/beneficiarios-siss-back/public/index.php/api/',
-  
-  
-
-};*/
-
-
-
 /* Producción */
 
 
@@ -26,12 +11,12 @@
 export const environment = {
   production: true,
   
-  apiUrl: 'http://vmwpser1.sidor.net/hcm-back/public/index.php/api/',
-  apiUrlPermanencia: 'http://vmwpcon1.sidor.net/permanencias-sql-back/public/index.php/api/',
-  apiUrlSeguridad: 'http://sirprdsvvapc21.sidor.net/seguridad-back/public/index.php/api/',
-  apiUrlLDAP: 'http://sirprdsvvapc21.sidor.net/ldap-back/public/index.php/api/',
-  apiUrlDocumentation: 'http://vmwpser1.sidor.net/manuales/',
-  apiUrlCargaFamiliar: 'http://vmwpser1.sidor.net/beneficiarios-siss-back/public/index.php/api/',
-  apiUrlSiss: 'http://vmwpser1.sidor.net/beneficiarios-siss-back/public/index.php/api/',
+  apiUrl: 'http://xxxxxxxxxxxxxx/hcm-back/public/index.php/api/',
+  apiUrlPermanencia: 'http://xxxxxxxxxxxxxx/permanencias-sql-back/public/index.php/api/',
+  apiUrlSeguridad: 'http://xxxxxxxxxxxxxx/seguridad-back/public/index.php/api/',
+  apiUrlLDAP: 'http://xxxxxxxxxxxxxx/ldap-back/public/index.php/api/',
+  apiUrlDocumentation: 'http://xxxxxxxxxxxxxx/manuales/',
+  apiUrlCargaFamiliar: 'http://xxxxxxxxxxxxxx/beneficiarios-siss-back/public/index.php/api/',
+  apiUrlSiss: 'http://xxxxxxxxxxxxxx/beneficiarios-siss-back/public/index.php/api/',
 };
 
